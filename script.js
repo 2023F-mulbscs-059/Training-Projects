dc17932e524a44671ba639ce03f19eb2310fdbd0
Atif Nasir <2023F-mulbscs-059@mul.edu.pk>
2025-05-22 19:23:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,4 @@ function showTab(tab, event) {
   tabs.forEach(t => t.classList.remove('active'));
   event.target.classList.add('active');
 
-  cards.forEach(card => {
-    const type = card.getAttribute('data-type');
-    if (type.includes(tab) || tab === 'all') {
-      card.classList.add('active');
-    } else {
-      card.classList.remove('active');
-    }
-  });
-}
 
