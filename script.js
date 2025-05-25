243ddc8c884998907f1202e4322c3254fb483b93
Atif Nasir <2023F-mulbscs-059@mul.edu.pk>
2025-05-25 18:42:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function showTab(tab, event) {
-  const cards = document.querySelectorAll('.card');
+ cards = document.querySelectorAll('.card');
   const tabs = document.querySelectorAll('.tab');
   tabs.forEach(t => t.classList.remove('active'));
   event.target.classList.add('active');
